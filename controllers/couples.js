d166bd9bf409eb9cb0e12d8c47f3bc2737430f04
Brittany Yarbrough <brittanymyar@gmail.com>
2022-10-03 17:00:45 -0400

    create events route

--- controllers/couples.js
+++ controllers/couples.js
@@ -25,18 +25,25 @@ couplesRouter.post('/', async (req, res) => {
 }); 
 
 // Couple/events Index route
-// couplesRouter.get('/:id', async (req, res) => {
-//     try {
-//         // send all people
-//         res.json(await Events.find({}));
-//     } catch (error) {
-//         //send error
-//         res.status(400).json(error);
-//     }
-// });
+couplesRouter.get('/:id', async (req, res) => {
+    try {
+        // send all people
+        res.json(await Events.find({}));
+    } catch (error) {
+        //send error
+        res.status(400).json(error);
+    }
+});
 
 
-
+// Couple/events Create route
+couplesRouter.post('/', async (req, res) => {
+    try {
+        res.json(await Events.create(req.body));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+}); 
 
 
 
